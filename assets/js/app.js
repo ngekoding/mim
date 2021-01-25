231371c1e78342a66aa3f12b9b329476c55f6336
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-01-25 18:47:03 +0800

    Improve UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,42 @@
 $(document).ready(function(){
   $.getJSON("assets/json/donations.json", function(data){
-      var lastUpdate = moment(data.lastUpdate).format("LLL");
+      var lastUpdate = moment(data.lastUpdate).format("LLL") + " WITA";
       
       var total = 0;
-      var tbody = "";
+      var donations = {};
+
+      // Gouping by date
       data.histories.forEach((item, index) => {
+        if (donations.hasOwnProperty(item.date)) {
+          donations[item.date].data.push(item);
+        } else {
+          donations[item.date] = {
+            date: item.date,
+            data: [item]
+          };
+        }
+
         total += item.amount;  
-        var row  = "<tr>";
-            row += '<td align="center">' + (index + 1) + '</td>'; 
-            row += '<td align="center">' + (moment(item.date).format("LL")) + '</td>'; 
-            row += '<td>' + (item.name) + '</td>'; 
-            row += '<td>' + (item.city) + '</td>'; 
-            row += '<td align="right">' + (item.amount.toLocaleString("id")) + '</td>'; 
-            row += "</tr>";
-        tbody += row;
       });
+
+      var tbody = "";
+      var num = 1;
+      for (var [key, value] of Object.entries(donations)) {
+        var rowspan = value.data.length;
+        var rowspanClass = rowspan > 1 ? "rowspan" : "";
+        value.data.forEach((item, index) => {
+          var row = "<tr>";
+          row += `<td align="center">${num++}</td>`;
+          if (index == 0) {
+            row += `<td rowspan="${rowspan}" align="center" class="${rowspanClass}">${moment(value.date).format("LL")}</td>`;
+          }
+          row += `<td>${item.name}</td>`;
+          row += `<td>${item.city}</td>`;
+          row += `<td align="right">${item.amount.toLocaleString("id")}</td>`;
+          row += "</tr>";
+          tbody += row;
+        });
+      }
 
       // Update the UI
       $('.donate-total').html("Rp" + total.toLocaleString("id"));
